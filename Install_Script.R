e97011fd087f1328c8041c45d37a8cb5d018e973
HamesThomas <hamesthomasbe@gmail.com>
2019-05-01 19:00:07 +0200

    Adding example of install.script

--- Install_Script.R
+++ Install_Script.R
@@ -5,23 +5,8 @@ path =  "/Users/ThomasHames/Documents/Sciences_Actuarielles/Mémoire/Code_Memoir
 if(!require(devtools)){install.packages("devtools")}
 if(!require(roxygen2)){devtools::install_github("klutometis/roxygen")}
 
-devtools::create("ExtendedMort2DSmooth")
-
-devtools::install_github("HamesThomas/Code_Memoire_Hames_Thomas")
-
-findmypath=function(dir,file){
-  path=system.file(dir,file,package="ExtendedMort2DSmooth")
-  return(path)
-}
-
-
-
-
-devtools::install("ExtendedMort2DSmooth")
-library("ExtendedMort2DSmooth")
-
-Mort2Dsmooth_Binomial_logit()
-
 
 devtools::install_github("HamesThomas/Code_Memoire_Hames_Thomas/ExtendedMort2DSmooth")
 library("ExtendedMort2DSmooth")
+
+# Mort2DSmooth_Binomial_logit, Mort2DSmooth_Binomial_cloglog,  Mort2DSmooth_poisson_log ready to use ! 
